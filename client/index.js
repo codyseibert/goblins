18ff2256f57cc47b21a1968cfe5b9fe53bb6a5e4
Cody Seibert <cseibert113@gmail.com>
2016-05-25 20:37:47 -0400

    random things

--- client/index.js
+++ client/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
+  $(window).resize(function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+  $(window).resize();
 
   var players = {};
+
+  var id = null;
+
+  var playerImg = new Image();
+  playerImg.src = "images/player.gif";
+
+  var crateImg = new Image();
+  crateImg.src = "images/crate.jpg";
 
   var A = 97;
   var D = 100;
@@ -13,6 +28,7 @@ $(document).ready(function(){
   };
 
   var input = {}
+  var map = []
 
   $(document).keydown(function(evt) {
     var key = evt.keyCode || evt.which;
@@ -31,29 +47,57 @@ $(document).ready(function(){
     socket.emit('join', 'Hello World from client');
   });
 
+  socket.on('map', function(m) {
+    map = m;
+  });
+
+  socket.on('id', function(i) {
+    id = i;
+  });
+
   socket.on('player', function(p) {
-    // console.log(player);
     if (!players[p.id]) {
       players[p.id] = p;
-      var $player = $('<div></div>')
-        .addClass('player');
-      players[p.id].$ = $player
-      $('body').append($player);
     }
 
     var player = players[p.id];
-    player.x = p.x;
-    player.y = p.y;
-    player.$.offset({
-      left: player.x,
-      top: player.y
-    });
 
+    _.extend(player, p);
+
+    render()
   });
 
   socket.on('player.disconnected', function(p) {
-    players[p.id].$.remove();
     delete players[p.id]
   });
+
+  function render() {
+    var cx, cy;
+    cx = cy = 0;
+    if (id) {
+      cx = parseInt(window.innerWidth / 2 - players[id].x, 10);
+      cy = parseInt(window.innerHeight / 2 - players[id].y, 10);
+    }
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (var id in players) {
+      var player = players[id]
+      var scaleX = player.isFacingLeft ? -1 : 1
+      var scale = 3.1
+      context.save();
+      context.translate(player.x + cx, player.y + cy);
+      context.scale(scaleX / scale, 1 / scale);
+      context.drawImage(playerImg, -150, -64);
+      context.restore();
+    }
+
+    for (var i in map) {
+      var crate = map[i]
+      context.save();
+      context.translate(crate.x + cx, crate.y + cy);
+      context.drawImage(crateImg, 0, 0);
+      context.restore();
+    }
+  }
 
 });
--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,26 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var js2dmath = require('js-2dmath');
+var Vec2 = js2dmath.Vec2;
+var Polygon = js2dmath.Polygon;
+var Intersection = js2dmath.Intersection;
+
+var Box = function(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+};
+
+var map = [
+  new Box(0, 300, 64, 64),
+  new Box(64, 300, 64, 64),
+  new Box(128, 300, 64, 64),
+  new Box(196, 300, 64, 64),
+  new Box(300, 300, 64, 64),
+  new Box(364, 300, 64, 64)
+]
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -25,16 +45,24 @@ var User = function (){
   this.vx = 0;
   this.vy = 0;
   this.id = null;
+  this.width = 50;
+  this.height = 50;
+  this.isFacingLeft = false;
   this.input = {
     left: false,
     right: false,
     jump: false
   };
+  this.canJump = false;
 };
 
 var disconnectQueue = []
 
 io.on('connection', function(client) {
+
+  client.emit('map', map);
+  client.emit('id', client.id);
+
   client.on('join', function() {
     users[client.id] = new User()
     users[client.id].id = client.id;
@@ -49,29 +77,65 @@ io.on('connection', function(client) {
   });
 });
 
+var isColliding = function(player, objects) {
+  for (var i = 0; i < objects.length; i++) {
+    obj = objects[i];
+    var boxPoly = Polygon.create(
+      Vec2.create(obj.x, obj.y),
+      Vec2.create(obj.x + obj.width, obj.y),
+      Vec2.create(obj.x + obj.width, obj.y + obj.height),
+      Vec2.create(obj.x, obj.y + obj.height)
+    )
+    var playerPoly = Polygon.create(
+      Vec2.create(player.x, player.y),
+      Vec2.create(player.x + player.width, player.y),
+      Vec2.create(player.x + player.width, player.y + player.height),
+      Vec2.create(player.x, player.y + player.height)
+    )
+    var intersection = Intersection.polygon_polygon(boxPoly, playerPoly);
+    if (intersection.reason === 8) {
+      return true;
+    }
+  }
+  return false;
+}
+
 setInterval(function(){
   // Handle Inputs & Update Positions
   var userIds = Object.keys(users);
   userIds.forEach(function(id) {
     var user = users[id];
     if (user.input.left) {
-      user.vx = -SPEED
+      user.vx = -SPEED;
+      user.isFacingLeft = true;
     } else if (user.input.right) {
-      user.vx = SPEED
+      user.vx = SPEED;
+      user.isFacingLeft = false;
     }
 
-    if (user.input.jump) {
-      user.y -= JUMP_OFFSET
-      user.vy = JUMP_SPEED
+    if (user.input.jump && user.canJump) {
+      user.y -= JUMP_OFFSET;
+      user.vy = JUMP_SPEED;
+      user.canJump = false;
     }
 
     user.vy += GRAVITY;
     user.vx *= FRICTION;
 
     user.x += user.vx;
-    user.y += user.vy;
+    if (isColliding(user, map)) {
+      user.x -= user.xy;
+    }
 
-    user.y = Math.min(300, user.y);
+    user.y += user.vy;
+    if (isColliding(user, map)) {
+      if (user.vy > 0) {
+        user.canJump = true;
+      }
+
+      user.y -= user.vy;
+      user.vy = 0;
+    }
 
     io.emit('player', user);
   });
